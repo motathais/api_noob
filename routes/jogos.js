7892440d005c50520b38b06e9835253db58510eb
Thais Marques Mota <thaismferreira42@gmail.com>
2024-10-16 23:12:25 -0300

    Update jogos.js
    alterada ordem dos middlewares

--- routes/jogos.js
+++ routes/jogos.js
@@ -12,11 +12,11 @@ router.route("/jogos").get((req, res) => jogoController.getAll(req,res));
 
 router.route("/jogos/:id").get((req, res) => jogoController.get(req,res));
 
-router.post("/jogos", checkToken, upload.single("file"), jogoController.create);
+router.post("/jogos", upload.single("file"), checkToken, jogoController.create);
 
 router.route("/jogos/:id").delete(checkToken, (req, res) => jogoController.delete(req, res)); 
 
-router.put("/jogos/:id", checkToken, upload.single("file"), jogoController.update); 
+router.put("/jogos/:id", upload.single("file"), checkToken, jogoController.update); 
 
 
 module.exports = router;
