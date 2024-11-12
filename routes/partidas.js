293a03370590fea12b95cfcda95308089d0fbc5c
Thais Marques Mota <thaismferreira42@gmail.com>
2024-11-12 20:23:07 -0300

    alterando a rota

--- routes/partidas.js
+++ routes/partidas.js
@@ -10,7 +10,7 @@ router.route("/partidas").post(checkToken, (req, res) => partidaController.creat
 
 router.route("/partidas").get(checkToken, (req, res) => partidaController.getAll(req, res)); 
 
-router.route("/partidas").get(checkToken, (req, res) => partidaController.get(req, res)); 
+router.route("/partidas/filtro").get(checkToken, (req, res) => partidaController.get(req, res)); 
 
 router.route("/partidas/:id").delete(checkToken, (req, res) => partidaController.delete(req, res));
 
